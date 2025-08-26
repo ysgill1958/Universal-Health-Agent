4656fc7f4d4b99007734dc428ff6d1d45d29f76c
ysgill1958 <131746761+ysgill1958@users.noreply.github.com>
2025-08-26 14:57:45 +0530

    Delete static directory

--- static/dir.js
+++
@@ -1,3 +0,0 @@
-fetch('./data/catalog.json').then(r=>r.json()).then(c=>{
-  document.getElementById('dir').textContent=JSON.stringify(c,null,2);
-});
--- static/plan.js
+++
@@ -1,3 +0,0 @@
-fetch('./data/longevity_plan.json').then(r=>r.json()).then(c=>{
-  document.getElementById('plan').textContent=JSON.stringify(c,null,2);
-});
